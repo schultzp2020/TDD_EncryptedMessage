e90107c83d091013019518ff1f0513edf9a72242
Paul Schultz <pschultz@pobox.com>
2021-11-04 14:58:27 -0500

    Initial commit

---
+++ TDD_Pair_EncyptedMessage/src/tdd/EncryptedMessage.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package tdd;
+
+/**
+ * @author TBD
+ *
+ */
+public class EncryptedMessage {
+
+	private String mEncryptedMessage;
+	/**
+	 * 
+	 */
+	public EncryptedMessage(String message, String key) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EncryptedMessage(String encryptedMessage) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getMessage() {
+		return mEncryptedMessage;
+	};
+	
+	public String decryptMessage(String key) {
+		return "";
+	}
+	
+	private Boolean validate(String data, Boolean allowSpaces) {
+		return false;
+	}
+
+	private void encryptMessage(String message, String key) {
+		
+	}
+}
